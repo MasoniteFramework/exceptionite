339efe9bdbc5a6d6e0fcbf6671e4549160857b47
Joseph Mancuso <idmann509@gmail.com>
2020-01-04 14:50:44 -0500

    bumped exceptionite to stable

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.13',
+    version='1.0.0',
     package_dir={'': 'src'},
 
     description='Exception Handling Made Easy',
@@ -29,7 +29,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
