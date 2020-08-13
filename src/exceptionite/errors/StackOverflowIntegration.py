9757c84a277e09b427e1a34ee08e03966d80fdfe
Samuel Girardin <samuelgirardin@gmail.com>
2020-08-13 10:50:03 +0200

    fix linting

--- src/exceptionite/errors/StackOverflowIntegration.py
+++ src/exceptionite/errors/StackOverflowIntegration.py
@@ -2,8 +2,13 @@ import os
 
 import jinja2
 import requests
-from jinja2 import (ChoiceLoader, DictLoader, Environment, PackageLoader,
-                    select_autoescape)
+from jinja2 import (
+    ChoiceLoader,
+    DictLoader,
+    Environment,
+    PackageLoader,
+    select_autoescape,
+)
 
 
 class StackOverflowIntegration:
@@ -15,50 +20,50 @@ class StackOverflowIntegration:
         if only_tags:
             self.tags = only_tags
         else:
-            self.tags = tags + ['python']
+            self.tags = tags + ["python"]
 
     def content(self, handler):
 
         try:
             response = requests.get(
                 "https://api.stackexchange.com/2.2/search/advanced?order=asc&sort=relevance&q='{0}'&body='{0}'&accepted=True&tagged={1}&site=stackoverflow".format(
-                    handler.message(), ';'.join(self.tags))
+                    handler.message(), ";".join(self.tags)
+                )
             ).json()
         except requests.exceptions.ConnectionError:
             return
 
-
         accepted_answer_ids = []
 
-        if not response.get('items'):
+        if not response.get("items"):
             response = requests.get(
-                'https://api.stackexchange.com/2.2/search?order=desc&sort=relevance&intitle={}&site=stackoverflow&filter=!-*jbN-(0_ynL&tagged={}&key=k7C3UwXDt3J0xOpri8RPgA(('.format(
-                    handler.exception(), ';'.join(self.tags))
+                "https://api.stackexchange.com/2.2/search?order=desc&sort=relevance&intitle={}&site=stackoverflow&filter=!-*jbN-(0_ynL&tagged={}&key=k7C3UwXDt3J0xOpri8RPgA((".format(
+                    handler.exception(), ";".join(self.tags)
+                )
             ).json()
 
-        for question in response.get('items', []):
-            if 'accepted_answer_id' in question:
-                accepted_answer_ids.append(str(question['accepted_answer_id']))
+        for question in response.get("items", []):
+            if "accepted_answer_id" in question:
+                accepted_answer_ids.append(str(question["accepted_answer_id"]))
 
-        self.count = len(response.get('items', []))
-        
+        self.count = len(response.get("items", []))
+
         answers = requests.get(
-            'https://api.stackexchange.com/2.2/answers/{}?order=desc&sort=activity&site=stackoverflow'.format(
-                ';'.join(accepted_answer_ids))
+            "https://api.stackexchange.com/2.2/answers/{}?order=desc&sort=activity&site=stackoverflow".format(
+                ";".join(accepted_answer_ids)
+            )
         ).json()
 
         current_path = os.path.dirname(os.path.abspath(__file__))
-        with open(os.path.join(current_path, 'templates/stackoverflow.html'), 'r') as f:
+        with open(os.path.join(current_path, "templates/stackoverflow.html"), "r") as f:
             overflow_exception = f.read()
 
-        loader = DictLoader({
-            'stackoverflow.html': overflow_exception,
-        })
+        loader = DictLoader({"stackoverflow.html": overflow_exception, })
 
         environment = Environment(
-            loader=loader,
-            autoescape=select_autoescape(['html', 'xml'])
+            loader=loader, autoescape=select_autoescape(["html", "xml"])
         )
 
-        return environment.get_template('stackoverflow.html').render({
-            'questions': response['items'], 'answers': answers})
+        return environment.get_template("stackoverflow.html").render(
+            {"questions": response["items"], "answers": answers}
+        )
