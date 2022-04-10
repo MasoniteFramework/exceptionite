6c4a7489dbe8ebe8dfb552cb01d9ea45909f1c9b
Joe Mancuso <idmann509@gmail.com>
2022-04-09 23:27:52 -0400

    fixed dark mode

--- src/exceptionite/assets/components/Sponsor.vue
+++ src/exceptionite/assets/components/Sponsor.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white dark:bg-gray-800 rounded-md p-2 w-full text-black mb-4 shadow-md">
+  <div class="bg-white dark:bg-gray-800 dark:text-gray-300 rounded-md p-2 w-full mb-4 shadow-md">
     <span style="text-decoration: underline">
         <a target="_blank" href="https://github.com/sponsors/MasoniteFramework">❤️ Find this page useful? Support Masonite so we can continue to build great free tools like this one!</a>
     </span>
